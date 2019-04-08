eb6bc7b9a07d2664395202e87d6dfb5baeffc03d
Arvin WW <ww.arvin@gmail.com>
2019-04-08 09:57:13 +0800

    no message

---
+++ src/main/java/com/cloud/zipkin/SpringCloudZipKinApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZipKinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZipKinApplication.class, args);
+	}
+
+}
